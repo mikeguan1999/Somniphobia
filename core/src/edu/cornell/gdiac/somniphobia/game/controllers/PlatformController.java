0b9dd60e46d8158783673607c38b7a165d986b34
JasonChen47 <42682065+JasonChen47@users.noreply.github.com>
2021-04-02 20:34:06 -0400

    Somehow added stuff from main here also

--- core/src/edu/cornell/gdiac/somniphobia/game/controllers/PlatformController.java
+++ core/src/edu/cornell/gdiac/somniphobia/game/controllers/PlatformController.java
@@ -112,6 +112,9 @@ public class PlatformController extends WorldController implements ContactListen
 	/** Texture for slider bars*/
 	private Texture sliderBarTexture;
 	private Texture sliderKnobTexture;
+	/** Texture for masking */
+	private TextureRegion circle_mask;
+	private Texture alpha_background;
 
 
 	/** Texture asset int for action*/
@@ -176,13 +179,28 @@ public class PlatformController extends WorldController implements ContactListen
 	private float LERP = 2f;
 
 
+	/** Masking stuff */
+	/** Dimensions for the mask when at its smallest */
+	Vector2 MIN_MASK_DIMENSIONS;
+	/** Max size for the mask to reach to extend off the screen TODO: replace with bounds checking*/
+	float MAX_MASK_SIZE;
+	/** Amount to increase and decrease rift mask size with */
+	float INCREMENT_AMOUNT;
+	/** Current width and height of the mask */
+	float maskWidth, maskHeight;
+	/** Whether or not the mask is in the process of switching*/
+	boolean switching;
+	/** Whether or not the mask is shrinking (switch occurred early on) */
+	boolean shrinking;
+	/** The character to perform the mask effect from */
+	CharacterModel maskLeader;
+
 	/** Mark set to handle more sophisticated collision callbacks */
-//	protected ObjectSet<Fixture> sensorFixtures;
 	protected ObjectSet<Fixture> lightSensorFixtures;
 	protected ObjectSet<Fixture> darkSensorFixtures;
 
 	protected ObjectSet<Fixture> combinedSensorFixtures;
-	//Platform logic
+	// Platform logic
 	/** This values so light only interacts with light and dark only interacts with dark*/
 	private final short CATEGORY_LPLAT = 0x0001;  //0000000000000001
 	private final short CATEGORY_DPLAT = 0x0002;  //0000000000000010
@@ -190,12 +208,10 @@ public class PlatformController extends WorldController implements ContactListen
 	private final short CATEGORY_PHOBIA = 0x0008;	   	  //0000000000001000
 	private final short CATEGORY_COMBINED = 0x0010; 	  //0000000000010000
 	private final short CATEGORY_ALLPLAT = 0x0020;
-//	private short all = 11111;
 
 	private final short MASK_LPLAT = CATEGORY_SOMNI | CATEGORY_COMBINED; //Collides with all
 
 	private final short MASK_DPLAT = CATEGORY_PHOBIA | CATEGORY_COMBINED;
-//		private final short MASK_DPLAT = -1 ;
 
 	private final short MASK_SOMNI = CATEGORY_LPLAT | CATEGORY_ALLPLAT;
 	private final short MASK_PHOBIA = CATEGORY_DPLAT | CATEGORY_ALLPLAT;
@@ -206,7 +222,6 @@ public class PlatformController extends WorldController implements ContactListen
 	Label.LabelStyle labelStyle;
 	private Slider [] sliders;
 	private Label [] labels;
-	//private Skin skin = new Skin(Gdx.files.internal("core/assets/shadeui/uiskin.atlas"));
 
 	public Widget sliderMenu;
 
@@ -222,12 +237,10 @@ public class PlatformController extends WorldController implements ContactListen
 	public PlatformController(int level) {
 
 		super(DEFAULT_WIDTH,DEFAULT_HEIGHT,DEFAULT_GRAVITY);
-//		System.out.println(MASK_DPLAT & CATEGORY_PHOBIA);
 		setDebug(false);
 		setComplete(false);
 		setFailure(false);
 		world.setContactListener(this);
-//		sensorFixtures = new ObjectSet<Fixture>();
 		lightSensorFixtures = new ObjectSet<Fixture>();
 		darkSensorFixtures = new ObjectSet<Fixture>();
 		combinedSensorFixtures = new ObjectSet<Fixture>();
@@ -246,7 +259,8 @@ public class PlatformController extends WorldController implements ContactListen
 		labels = new Label[7];
 		CharacterModel avatar = movementController.getAvatar();
 
-		Stage stage = new Stage(new ScreenViewport());
+
+		Stage stage = new Stage(new ScreenViewport(camera));
 //		Table table= new Table();
 		Batch b = canvas.getBatch();
 		ChangeListener slide = new ChangeListener() {
@@ -473,9 +487,9 @@ public class PlatformController extends WorldController implements ContactListen
 		Batch b = canvas.getBatch();
 		for (int i = 0; i < sliders.length; i++) {
 			Slider s = sliders[i];
-			s.setPosition(camera.position.x - canvas.getWidth()/2.5f, s.getY());
+			s.setPosition(camera.position.x - canvas.getWidth()/2.5f, 57*i + camera.position.y - canvas.getHeight()/3f);
 			Label l= labels[i];
-			l.setPosition(camera.position.x - canvas.getWidth()/2.5f, l.getY());
+			l.setPosition(camera.position.x - canvas.getWidth()/2.5f, 57*i + 32 +  + camera.position.y - canvas.getHeight()/3f);
 
 			l.draw(b, 1.0f);
 			s.draw(b, 1.0f);
@@ -590,18 +604,22 @@ public class PlatformController extends WorldController implements ContactListen
 		TextureRegion [] phobiasomnis = {phobiaSomniTexture,phobiaSomniWalkTexture,phobiaSomniDashSideTexture,phobiaSomniDashUpTexture, phobiaSomniDashUpTexture};
 		phobiasomnisTexture = phobiasomnis;
 
+		// Setup masking
+		circle_mask = new TextureRegion(directory.getEntry("circle_mask",Texture.class));
+		Vector2 mask_size = new Vector2(circle_mask.getRegionWidth(), circle_mask.getRegionHeight());
+		MIN_MASK_DIMENSIONS = new Vector2(mask_size).scl(0.125f);
+		maskWidth = MIN_MASK_DIMENSIONS.x;
+		maskHeight = MIN_MASK_DIMENSIONS.y;
+		MAX_MASK_SIZE = MIN_MASK_DIMENSIONS.x * 22.5f;
+		INCREMENT_AMOUNT = 50;
 
-//		somnisTexture = new TextureRegion[]{somniTexture,somniWalkTexture,somniDashSideTexture,somniDashUpTexture, somniFallTexture};
-//		phobiasTexture = new TextureRegion[]{phobiaTexture,phobiaWalkTexture,phobiaDashSideTexture,phobiaDashUpTexture, phobiaFallTexture};
-//		somniphobiasTexture = new TextureRegion[]{somniPhobiaTexture,somniPhobiaWalkTexture,somniPhobiaDashSideTexture,somniPhobiaDashUpTexture, somniPhobiaDashUpTexture};
-//		phobiasomnisTexture = new TextureRegion[]{phobiaSomniTexture,phobiaSomniWalkTexture,phobiaSomniDashSideTexture,phobiaSomniDashUpTexture, phobiaSomniDashUpTexture};
+
 		AssetDirectory internal = new AssetDirectory( "loading.json" );
 		internal.loadAssets();
 		internal.finishLoading();
 
 		sliderBarTexture = directory.getEntry( "platform:sliderbar", Texture.class);
 		sliderKnobTexture = directory.getEntry( "platform:sliderknob", Texture.class);
-
 
 		jumpSound = directory.getEntry( "platform:jump", SoundBuffer.class );
 		fireSound = directory.getEntry( "platform:pew", SoundBuffer.class );
@@ -642,14 +660,15 @@ public class PlatformController extends WorldController implements ContactListen
 		backgroundTexture = backgroundLightTexture;
 //		avatar = phobia;
 		lead = phobia;
+		maskLeader = somni;
 
 		world = new World(gravity,false);
 		world.setContactListener(this);
 		setComplete(false);
 		setFailure(false);
 		populateLevel(level);
-//		movementController = new MovementController(somni, phobia, combined, goalDoor, objects, sharedObjects, this);
-//		movementController.setAvatar(phobia);
+		movementController = new MovementController(somni, phobia, combined, goalDoor, objects, sharedObjects, this);
+		movementController.setAvatar(phobia);
 
 	}
 
@@ -794,11 +813,18 @@ public class PlatformController extends WorldController implements ContactListen
 
 		action = 0;
 
-		movementController = new MovementController(somni, phobia, combined, goalDoor, objects, sharedObjects, this);
 
-		//Set current avatar to Somni
-//		avatar = somni;
-		movementController.setAvatar(somni);
+//		movementController = new MovementController(somni, phobia, combined, goalDoor, objects, sharedObjects, this);
+//
+//		//Set current avatar to Somni
+////		avatar = somni;
+//		movementController.setAvatar(somni);
+
+		//Set current avatar to Phobia
+		avatar = phobia;
+		maskLeader = somni;
+
+
 		volume = constants.getFloat("volume", 1.0f);
 	}
 
@@ -843,6 +869,7 @@ public class PlatformController extends WorldController implements ContactListen
 		somni = movementController.getSomni();
 		phobia = movementController.getPhobia();
 		avatar = movementController.getAvatar();
+
 
 		if (movementController.getSwitchedCharacters()) {
 			backgroundTexture = backgroundTexture == backgroundLightTexture ?
@@ -980,6 +1007,7 @@ public class PlatformController extends WorldController implements ContactListen
 			if (lightSensorFixtures.size == 0) {
 				somni.setGrounded(false);
 			}
+
 		}
 		if ((phobia.getSensorName().equals(fd2) && phobia != bd1 && goalDoor != bd1) ||
 				(phobia.getSensorName().equals(fd1) && phobia != bd2 && goalDoor != bd2)) {
@@ -1000,25 +1028,145 @@ public class PlatformController extends WorldController implements ContactListen
 	}
 
 	/**
+	 * Draws the necessary textures to mask properly.
+	 * @param cameraX The x-coord for the camera origin
+	 * @param cameraY The y-coord for the camera origin
+	 * @param maskWidth The width of the mask
+	 * @param maskHeight The height of the mask
+	 * @param character The character to center the mask on
+	 */
+	public void drawMask(float cameraX, float cameraY, float maskWidth, float maskHeight, CharacterModel character) {
+		character = holdingHands ? combined : character;
+		float leadCenterX = character.getX() * canvas.PPM + character.getWidth() / 2 - maskWidth / 2;
+		float leadCenterY = character.getY() * canvas.PPM + character.getHeight() / 2 - maskHeight / 2;
+		canvas.beginCustom(GameCanvas.BlendState.OPAQUE, GameCanvas.ChannelState.ALPHA);
+		if(alpha_background == null) {
+			Pixmap pixmap=new Pixmap(canvas.getWidth(), canvas.getHeight(), Pixmap.Format.RGBA8888);
+			pixmap.setColor(Color.CLEAR);
+			pixmap.fillRectangle(0,0, pixmap.getWidth(), pixmap.getHeight());
+			alpha_background = new Texture(pixmap);
+		}
+		canvas.draw(alpha_background, Color.WHITE, cameraX, cameraY, canvas.getWidth(), canvas.getHeight());
+		canvas.draw(circle_mask, Color.WHITE, leadCenterX, leadCenterY, maskWidth, maskHeight);
+		canvas.endCustom();
+	}
+
+	/**
+	 * Draws the necessary textures for the character's realm rift.
+	 * @param cameraX The x-coord for the camera origin
+	 * @param cameraY The y-coord for the camera origin
+	 * @param character The character whose environment is being drawn
+	 */
+	public void drawCharacterRift(float cameraX, float cameraY, CharacterModel character) {
+		canvas.beginCustom(GameCanvas.BlendState.NO_PREMULT_DST, GameCanvas.ChannelState.ALL);
+		TextureRegion background = character.equals(somni) ? backgroundDarkTexture : backgroundLightTexture;
+		canvas.draw(background, Color.WHITE, cameraX, cameraY, canvas.getWidth(), canvas.getHeight());
+		canvas.endCustom();
+	}
+
+	/**
+	 * Draws the necessary textures for the character's platforms.
+	 * @param character The character whose environment is being drawn
+	 */
+	public void drawCharacterPlatform(CharacterModel character) {
+		canvas.beginCustom(GameCanvas.BlendState.NO_PREMULT_DST, GameCanvas.ChannelState.ALL);
+		PooledList<Obstacle> objects = character.equals(somni) ? lightObjects : darkObjects;
+		for(Obstacle obj : objects) {
+			obj.draw(canvas);
+		}
+		canvas.endCustom();
+	}
+
+	/**
 	 * Draw the physics objects together with foreground and background
 	 *
 	 * This is completely overridden to support custom background and foreground art.
 	 *
 	 * @param dt Timing values from parent loop
 	 */
-	float previousValue = 0;
-	float lastValue = 0;
 	public void draw(float dt) {
 
 		CharacterModel avatar = movementController.getAvatar();
 		canvas.setCamera(camera);
 		canvas.clear();
 
-		// Draw background unscaled.
+		float cameraX = camera.position.x - canvas.getWidth() / 2;
+		float cameraY = camera.position.y - canvas.getHeight() / 2;
+
+		// Draw background
+		canvas.beginCustom(GameCanvas.BlendState.NO_PREMULT_DST, GameCanvas.ChannelState.ALL);
+		canvas.draw(backgroundTexture, Color.WHITE, cameraX, cameraY, canvas.getWidth(), canvas.getHeight());
+		canvas.endCustom();
+
+		drawMask(cameraX, cameraY, maskWidth, maskHeight, maskLeader);
+		drawCharacterRift(cameraX, cameraY, maskLeader);
+		drawCharacterPlatform(maskLeader);
+
+		CharacterModel follower = lead.equals(phobia) ? somni : phobia;
+		// Check if switching and update mask drawing
+		if(switching) {
+			maskWidth += maskWidth >= MAX_MASK_SIZE ? 0 : INCREMENT_AMOUNT;
+			maskHeight += maskHeight >= MAX_MASK_SIZE ? 0 : INCREMENT_AMOUNT;
+			if(maskWidth >= MAX_MASK_SIZE) {
+				maskWidth = MIN_MASK_DIMENSIONS.x;
+				maskHeight = MIN_MASK_DIMENSIONS.y;
+				switching = false;
+				maskLeader = follower;
+				//System.out.println(follower.equals(somni) ? "Somni" : "Phobia");
+				backgroundTexture = backgroundTexture.equals(backgroundLightTexture) ? backgroundDarkTexture :
+						backgroundLightTexture;
+			}
+			drawMask(cameraX, cameraY, MIN_MASK_DIMENSIONS.x, MIN_MASK_DIMENSIONS.y, follower);
+			drawCharacterRift(cameraX, cameraY, follower);
+			drawCharacterPlatform(follower);
+		} else {
+			/*if(maskWidth == MIN)
+			if(shrinking) {
+				drawMask(cameraX, cameraY, maskWidth, maskHeight, maskLeader);
+				drawCharacterRift(cameraX, cameraY, maskLeader);
+				drawCharacterPlatform(maskLeader);
+			}*/
+
+			// Draw lead platform
+			canvas.begin();
+			for(Obstacle obj : lead.equals(somni) ? lightObjects : darkObjects) {
+				obj.draw(canvas);
+			}
+			canvas.end();
+
+			// Draw follower platforms if holding hands
+			canvas.begin();
+			if(holdingHands) {
+				for(Obstacle obj : lead.equals(somni) ? darkObjects : lightObjects) {
+					obj.draw(canvas);
+				}
+			}
+			canvas.end();
+			maskWidth -= maskWidth <= MIN_MASK_DIMENSIONS.x ? 0 : INCREMENT_AMOUNT;
+			maskHeight -= maskHeight <= MIN_MASK_DIMENSIONS.y ? 0 : INCREMENT_AMOUNT;
+		}
+		// Draw shared platforms
 		canvas.begin();
-		float x = camera.position.x - canvas.getWidth() / 2;
-		float y = camera.position.y - canvas.getHeight() / 2;
-		canvas.draw(backgroundTexture, Color.WHITE, x, y, canvas.getWidth() ,canvas.getHeight());
+		for(Obstacle obj : sharedObjects) {
+			// Ignore characters which we draw separately
+			if (!(obj instanceof CharacterModel)) {
+				obj.draw(canvas);
+			}
+		}
+		canvas.end();
+
+		// Draw current model
+		canvas.begin();
+		if(holdingHands) {
+			combined.draw(canvas);
+		} else {
+			follower.draw(canvas);
+			lead.draw(canvas);
+		}
+		canvas.end();
+
+		// Draw sliders if active
+		canvas.begin();
 		if (slidersActive()) {
 			if (tes == 0) {
 				createSliders();
@@ -1033,53 +1181,8 @@ public class PlatformController extends WorldController implements ContactListen
 		}
 		canvas.end();
 
-
-		if(avatar == somni){
-			canvas.begin();
-			for(Obstacle obj : sharedObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-			canvas.begin();
-			for(Obstacle obj : lightObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-		}else if (avatar == phobia){
-			canvas.begin();
-			for(Obstacle obj : sharedObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-			canvas.begin();
-			for(Obstacle obj : darkObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-		}else{
-			canvas.begin();
-			for(Obstacle obj : sharedObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-			canvas.begin();
-			for(Obstacle obj : lightObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-			canvas.begin();
-			for(Obstacle obj : darkObjects) {
-				obj.draw(canvas);
-			}
-			canvas.end();
-		}
-
+		// Draw debug if active
 		if (isDebug()) {
-			/*canvas.beginDebug();
-			for(Obstacle obj : objects) {
-				obj.drawDebug(canvas);
-			}
-			canvas.endDebug();*/
 			canvas.beginDebug();
 			for(Obstacle obj : sharedObjects) {
 				obj.drawDebug(canvas);
@@ -1097,7 +1200,8 @@ public class PlatformController extends WorldController implements ContactListen
 			canvas.endDebug();
 
 		}
-		// Final message
+
+		// Draw final message when level ends
 		if (isComplete() && !isFailure()) {
 			displayFont.setColor(Color.YELLOW);
 			canvas.begin(); // DO NOT SCALE
@@ -1111,7 +1215,6 @@ public class PlatformController extends WorldController implements ContactListen
 			displayFont.getData().setScale(1f, 1f);
 
 			canvas.drawTextCameraCentered("FAILURE!", displayFont, camera.position.x, camera.position.y);
-//			canvas.drawTextCentered("FAILURE!", displayFont, 0.0f);
 			canvas.end();
 		}
 	}
