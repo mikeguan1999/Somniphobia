a5631b990f44a30d5ed662389138eb9f3e82b9b7
mikeguan1999 <mikeguan1999@gmail.com>
2021-04-11 12:19:37 -0700

    LevelCreator

--- core/src/edu/cornell/gdiac/somniphobia/game/controllers/LevelCreator.java
+++ core/src/edu/cornell/gdiac/somniphobia/game/controllers/LevelCreator.java
@@ -1,6 +1,41 @@
 package edu.cornell.gdiac.somniphobia.game.controllers;
 
+import edu.cornell.gdiac.util.PooledList;
+
 public class LevelCreator {
+    class Platform {
+        int posX;
+        int posY;
+        int width;
+        int height;
+        public Platform(int posX, int posY, int width, int height) {
+            this.posX = posX;
+            this.posY = posY;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    class Level {
+        int width;
+        int height;
+        PooledList<Platform> platformList;
+        public Level(PooledList<Platform> platformList) {
+            this.platformList = platformList;
+        }
+        // TODO: Add platform
+        public void addPlatform() {
+
+        }
+        // TODO: Delete platform
+        public void deletePlatform() {
+
+        }
+    }
+
+    public LevelCreator() {
+        //TODO
+    }
 
     // TODO: Implement
 
